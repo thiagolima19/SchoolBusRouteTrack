3476b090521342573eb9d7b544817efeab9bc0b7
Thiago Lima <thiago_lima19@hotmail.com>
2025-12-05 12:45:17 -0700

    creation of Trip, currentUser and LoginUser class to handle the trip, current user and login

---
+++ SchoolBusRouteTrack/TripModels/Trip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolBusRouteTrack.TripModels
+{
+    public class Trip
+    {
+        public int TripID { get; set; }
+        public int RouteID { get; set; }
+        public int DriverID { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string Status { get; set; }
+        public string RouteNumber { get; set; }      
+        public string RouteDescription { get; set; } 
+        public string SchoolName { get; set; }       
+    }
+}
---
+++ SchoolBusRouteTrack/UserModel/LoginUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolBusRouteTrack.UserModel
+{
+    //gets and sets the Login user information that comes from the DB
+    public class LoginUser
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public int? DriverID { get; set; }
+    }
+}
